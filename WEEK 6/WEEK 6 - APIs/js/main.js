0e4a72819ca69465004d478dc87267cbb53da9ef
cbandj <151044882+cbandj@users.noreply.github.com>
2025-03-06 16:06:16 -0500

    Add files via upload

---
+++ WEEK 6/WEEK 6 - APIs/js/main.js
@@ -0,0 +1,47 @@
+//Then lets make two triangle objects:
+window.onload = function () {
+  let canvas = document.getElementById("testCanvas");
+  //get the context
+  let context = canvas.getContext("2d");
+
+  let triangleA = new Triangle( // calls triangle class to make tri1
+    canvas.width / 2,
+    canvas.height / 2,
+    50,
+    "#FFFFFF",
+    "#156cc8",
+    2,
+    1,
+    1
+  );
+  triangleA.display(context);
+
+   let triangleB = new Triangle(// calls triangle class to make tri2
+    400,
+    10,
+    25,
+    "#FFFFFF",
+    "#C81582",
+    2,
+    -1,
+    1
+  );
+  triangleB.display(context);
+
+  requestAnimationFrame(animate);
+
+  function animate() {
+    //repaint with a black rect..
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    triangleA.setPoints();
+    triangleA.update();
+    triangleA.display(context);
+    
+    triangleB.setPoints();
+    triangleB.update();
+    triangleB.display(context);
+
+    requestAnimationFrame(animate);
+  }
+  
+};
